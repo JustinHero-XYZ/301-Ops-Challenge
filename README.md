086bf5e9e9735b15afcdc1c48768b859d6bb18d9
JustinHero-XYZ <justin.hero.2024@gmail.com>
2024-02-22 22:17:21 +0000

    301w10-ops-challenge-03

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # 301-Ops-Challenge
 
-Ops challenges are located in the 301-Ops-Challenge Wiki!
+#Ops challenges are located in the 301-Ops-Challenge Wiki!
+
+#Bash Template
+
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
---
+++ permissions.sh
@@ -0,0 +1,21 @@
+#1/bin/bash
+
+# Script Name: permissions.sh
+# Author: Justin Patterson
+# Date of latest revision: 02/22/2024
+# Purpose: change permissions
+
+# Prompt user for input directory path
+echo "Enter directory path:"
+read directory_path
+
+# Prompt user for input permissions number
+echo "Enter the permissions number (e.g. 777):"
+read permissions
+
+# Change permissions for files in the directory
+chmod -R "$permissions" "$directory_path"
+
+# Print directory contents and new permission settings
+echo "Directory contents:"
+ls -l "$directory_path"
