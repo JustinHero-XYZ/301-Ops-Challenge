617cf024a818ff80544c462dbce2a98d5c9b5987
JustinHero-XYZ <justin.hero.2024@gmail.com>
2024-02-23 06:21:07 +0000

    setup

---
+++ Conditionals_301w10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
--- clearing_logs_301w10.sh
+++ clearing_logs_301w10.sh
@@ -1,23 +1,6 @@
-# Ops-Challenge-301w10
-
-#Ops challenges are located in the 301-Ops-Challenge Wiki!
-
-#Bash Template
-
 #!/bin/bash
 
 # Script Name:                  Script Name
 # Author:                       Your Name
 # Date of latest revision:      00/00/0000
 # Purpose:                      Purpose
-
-# Declaration of variables
-
-# Declaration of functions
-
-
-# Main
-
-
-
-# End
