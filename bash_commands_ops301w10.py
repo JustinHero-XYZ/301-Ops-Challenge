e4a563d5cada5a1652b53fefd090fd5a9ca82ec4
JustinHero-XYZ <justin.hero.2024@gmail.com>
2024-03-02 22:20:22 +0000

    bash_commands_ops301w10.py

---
+++ bash_commands_ops301w10.py
@@ -0,0 +1,21 @@
+import os
+
+# Declare and reference variables
+command1 = "whoami"
+command2 = "ip a"
+command3 = "lshw -short"
+
+# Execute bash commands usind os.system()
+print("Executing bash commands:")
+
+# Execute 'whoami' command
+print("\nOutput of 'whoami' command:")
+os.system(command1)
+
+# Execute 'ip a' command
+print("\nOutput of 'ip a' command:")
+os.system(command2)
+
+# Execute 'lshw -short' command
+print("n\Output of 'lshw -short' command:")
+os.system(command3)
