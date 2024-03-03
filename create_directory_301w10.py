140bfe9bd50e68ed7b02a865b0add097cb57319e
JustinHero-XYZ <justin.hero.2024@gmail.com>
2024-03-03 23:17:47 +0000

    ops challenge revision(name+date)

--- create_directory_301w10.py
+++ create_directory_301w10.py
@@ -1,8 +1,8 @@
-# Author:
+# Author: Justin Patterson
 # Date last revised:
-# Ops Challenge #: 
-# Purpose:
-# Script Name:
+# Ops Challenge #: Seattle-Ops-301w10 Ops Challenge 07
+# Purpose: Create Directory using Python
+# Script Name: create_directory_301w10.py
 
 # Variables
 
