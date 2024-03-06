6654525744a5656edb3fd0be1bd3e70390a308be
JustinHero-XYZ <justin.hero.2024@gmail.com>
2024-03-06 17:29:33 +0000

    python_lists_301w10.py

---
+++ python_lists_301w10.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Author: Justin Patterson
+# Date last revised: 03/06/2024
+# Ops Challenge #: Seattle-Ops-301w10 Ops Challenge 08
+# Purpose: How to work with lists in Python
+# Script Name: python_lists_301w10.py
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+### Assign a list of ten string elements to a variable
+my_list = ["apple", "banana", "orange", "grape", "kiwi", "pear", "peach", "plum", "cherry", "melon"]
+
+### Read user input here into a variable
+
+# Declaration of functions
+
+### Declare a function here
+
+# Main
+
+### Print the fourth element of the list
+print("Fourth element:", my_list[3])
+
+### Print the sixth element of the list
+print("Sixth through tenth elements:", my_list[5])
+
+### Change the value of the seventh element to "onion"
+my_list[6] = "onion"
+
+### Print the modified list
+print("Modified list:", my_list)
+
+### Pass the variable into the function here
+
+# End
