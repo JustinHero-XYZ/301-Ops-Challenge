317a72c1495234c6144b211bf21954a7b223ca04
JustinHero-XYZ <justin.hero.2024@gmail.com>
2024-04-03 16:36:26 +0000

    python_lists_301d12.py

--- python_lists_301d12.py
+++ python_lists_301d12.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 # Author: Justin Patterson
-# Date last revised: 03/06/2024
-# Ops Challenge #: Seattle-Ops-301w10 Ops Challenge 08
+# Date last revised: 04/02/2024
+# Ops Challenge #: Seattle-Ops-301d12 Ops Challenge 08
 # Purpose: How to work with lists in Python
-# Script Name: python_lists_301w10.py
+# Script Name: python_lists_301d12.py
 
 # Import libraries
 
@@ -13,12 +13,6 @@ import os
 # Declaration of variables
 ### Assign a list of ten string elements to a variable
 my_list = ["apple", "banana", "orange", "grape", "kiwi", "pear", "peach", "plum", "cherry", "melon"]
-
-### Read user input here into a variable
-
-# Declaration of functions
-
-### Declare a function here
 
 # Main
 
