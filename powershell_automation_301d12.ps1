0874e54488bad61f9cec38da7b1a7d5eb4a4f6a1
JustinHero-XYZ <justin.hero.2024@gmail.com>
2024-04-10 18:04:20 +0000

    powershell_automation_301d12.ps1

--- powershell_automation_301d12.ps1
+++ powershell_automation_301d12.ps1
@@ -1,12 +1,19 @@
 ###Powershell Interpreter###
 
 # Author: Justin Patterson
-# Date last revised: 03/14/2024
-# Ops Challenge #: Seattle-Ops-301w10 Ops Challenge 13
+# Date last revised: 04/10/2024
+# Ops Challenge #: Seattle-Ops-301d12 Ops Challenge 13
 # Purpose: Powershell Automation script that adds to AD
-# Script Name: powershell_automation_301w10.ps1
+# Script Name: powershell_automation_301d12.ps1
+
+# Define user details
+$Name = Read-Host "Enter Name of New User:"
+$Title = Read-Host "Enter Title of New User:"
+$Office = Read-Host "Enter Office Location:" 
+$Department = Read-Host "Enter New User Department:"
+$EmailAddress = "Enter New User Email:"
 
 # Add user, title, office, department, email
-New-ADUser -Name "Fraz Ferdinand" -Title "TPS Reporting Lead" -Office "Springfield, OR" -Department "TPS" -EmailAddress "franzferdinand@globexpower.com"
+New-ADUser -Name "$Name" -Title "$Title" -Office "$Office" -Department "$Department" -EmailAddress "$EmailAddress"
 
 # End
